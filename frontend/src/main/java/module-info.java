1573de7c9cbf851d76b1450ccf88753d33d059b8
erumshaukat <erum_shaukat@yahoo.com>
2023-09-20 10:54:33 +0200

    update java version for sonar

--- frontend/src/main/java/module-info.java
+++ frontend/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module de.iav.frontend {
     requires javafx.controls;
     requires javafx.fxml;
-            
+
                             
     opens de.iav.frontend to javafx.fxml;
     exports de.iav.frontend;
+
+
+
 }
